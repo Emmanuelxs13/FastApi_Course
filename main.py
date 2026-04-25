aa970800f0266ae670c7ef71fa600867eee3a902
Emmanuel Berrio <emmanuelberriojimenez13@gmail.com>
2026-04-24 22:09:08 -0500

    Refactor movie data structure and update movie retrieval endpoints

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
+movies = [
+
+    {
+        "id": 1,
+        "title": "Inception",
+        "year": 2010,
+        "genre": "Sci-Fi"
+    },
+    {
+        "id": 2,
+        "title": "The Godfather",
+        "year": 1972,
+        "genre": "Crime"
+    },
+    {
+        "id": 3,
+        "title": "Spirited Away",
+        "year": 2001,
+        "genre": "Animation"
+    }
+
+]
+
 app = FastAPI()
+
 
 @app.get('/', tags=['Home'])
 def home():
@@ -9,5 +33,13 @@ def home():
 
 
 @app.get('/movies', tags=['Home'])
-def home():
-    return HTMLResponse(content="<h1>Movies</h1><p>List of movies will be here.</p>", status_code=200)
+def get_movies():
+    return movies
+
+
+@app.get('/movies/{id}', tags=['Home'])
+def get_movie(id: int):
+    for movie in movies:
+        if movie['id'] == id:
+            return movie
+    return {"error": "Movie not found"}
