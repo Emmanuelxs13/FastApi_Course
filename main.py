1f0e8f2f110acb99a478f4067caf4d4585b242d3
Emmanuel Berrio <emmanuelberriojimenez13@gmail.com>
2026-04-29 18:51:30 -0500

    Refactor movie API endpoints to enhance response handling and add file response functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import Body, FastAPI, Path, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse, RedirectResponse, FileResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
 
@@ -8,16 +8,18 @@ app = FastAPI()
 
 movies: List[Movie] = []
 
+
 class Movie(BaseModel):
     id: int
     title: str
     year: int
     category: str
 
+
 class MovieCreate(BaseModel):
     id: int
     title: str = Field(min_length=5, max_length=15)
-    year: int = Field(ge=1900, le=2024) 
+    year: int = Field(ge=1900, le=2024)
     category: str = Field(min_length=5, max_length=20)
 
     model_config = {
@@ -30,6 +32,8 @@ class MovieCreate(BaseModel):
             }
         }
     }
+
+
 class MovieUpdate(BaseModel):
     title: str
     year: int
@@ -38,49 +42,58 @@ class MovieUpdate(BaseModel):
 
 @app.get('/', tags=['Home'])
 def home():
-    return "Hello world"
+    return PlainTextResponse(content='Welcome to the Movie API!')
 
 
 @app.get('/movies', tags=['Home'])
 def get_movies() -> List[Movie]:
-    return [movie.model_dump() for movie in movies]
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
 
 
 @app.get('/movies/{id}', tags=['Movies'])
 def get_movie(id: int = Path(gt=0)) -> Movie | dict:
     for movie in movies:
         if movie['id'] == id:
-            return movie.model_dump()
-    return {}
+            return JSONResponse(content=movie.model_dump())
+    return JSONResponse(content={})
 
 
 @app.get('/movies/', tags=['Movies'])
 def get_movie_by_category(category: str = Query(min_length=5, max_length=20)) -> Movie | dict:
     for movie in movies:
         if movie.category == category:
-            return movie.model_dump()
-        return {}
+            return JSONResponse(content=movie.model_dump())
+    return JSONResponse(content={})
 
 
 @app.post('/movies', tags=['Movies'])
 def create_movie(movie: MovieCreate) -> List[Movie]:
     movies.append(movie)
-    return [movie.model_dump() for movie in movies]
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
+    # return RedirectResponse(url='/movies', status_code=303)
 
 
 @app.put('/movies/{id}', tags=['Movies'])
 def update_movie(id: int, movie: MovieUpdate) -> List[Movie]:
     for item in movies:
-        if item['id'] == id:
-            item['title'] = movie.title
-            item['year'] = movie.year
-            item['category'] = movie.category
-            return [movie.model_dump() for movie in movies]
+        if item.id == id:
+            item.title = movie.title
+            item.year = movie.year
+            item.category = movie.category
+            content = [movie.model_dump() for movie in movies]
+            return JSONResponse(content=content)
 
 
 @app.delete('/movies/{id}', tags=['Movies'])
 def delete_movie(id: int) -> List[Movie]:
     for movie in movies:
-        if movie['id'] == id:
+        if movie.id == id:
             movies.remove(movie)
-            return [movie.model_dump() for movie in movies]
+            content = [movie.model_dump() for movie in movies]
+            return JSONResponse(content=content)
+
+@app.get('/get_file')
+def get_file():
+    return FileResponse(path='sample.txt', media_type='text/plain', filename='sample.txt')
